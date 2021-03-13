9d3ffc67449e80b8bd297f4e0057e2be52e8e413
boo-standing-up <70996914+boo-standing-up@users.noreply.github.com>
2021-03-13 09:22:37 +0900

    スブレット構文

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-import "./styles.css";
+/**
+ * 分割代入
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const booBody = {
+//   name: "boo",
+//   age: 14
+// };
+
+// const { name, age } = booBody;
+// const look2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(look2);
+
+/**
+ * デフォルト値
+ */
+
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
+// sayHello();
+
+/**
+ * スブレット構文
+ */
+
+//配列の展開
+
+// const hairetu = ["boo1", "boo2"];
+// // console.log(hairetu);
+// // console.log(...hairetu);
+
+// const className = (people1, people2) =>
+//   console.log(`クラス名簿${people1}クラス名簿${people2}` + people2);
+// className(...hairetu);
+
+//まとめる
+
+// const className = ["boo", "boo1", "boo2", "boo3", "boo4"];
+// const [na1,na2,...className3] = className;
+// console.log(na1);
+// console.log(className3);
+
+//コピーと結合
+const ichikumi = ["tarou", "jirou", "saburou", "boo"];
+const nikumi = ["hanako", "makko", "booko", "kayoko"];
+const classAll = [...ichikumi, ...nikumi];
+console.log(classAll);
